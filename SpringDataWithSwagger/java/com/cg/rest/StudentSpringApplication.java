7358b50d5e642c7e958245753cd42397ef7073ee
Sayan Chatterjee <sayanchatterjee029@gmail.com>
2021-09-20 12:24:27 +0530

    Spring

---
+++ SpringDataWithSwagger/java/com/cg/rest/StudentSpringApplication.java
@@ -0,0 +1,14 @@
+package com.cg.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSpringApplication.class, args);
+
+	}
+
+}
